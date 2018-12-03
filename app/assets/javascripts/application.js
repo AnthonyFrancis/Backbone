9cb0eac61eff307abde26ff7cb05b3b347081c4e
AnthonyFrancis <tonytrack@gmail.com>
2018-12-03 20:51:28 +0000

    bundle install, all ready to push to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def features
+  end
+
+  def contact
+  end
+end
